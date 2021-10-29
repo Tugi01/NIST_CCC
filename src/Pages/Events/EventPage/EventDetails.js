46145e3fbfc2feea8404ef216469bb9530261089
Debarshi <debopiku1122@gmail.com>
2021-10-29 10:32:27 +0530

    update_events

--- src/Pages/Events/EventPage/EventDetails.js
+++ src/Pages/Events/EventPage/EventDetails.js
@@ -1,4 +1,4 @@
-import React, { useEffect,useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useLocation } from 'react-router'
 
 
@@ -22,8 +22,28 @@ const EventDetails = () => {
 
   return (
     <div className="mt-5 text-center" style={{ marginBottom: '15%' }}>
-      <section>
-        HI <b>{state}</b>
+      <section className="event--details--section">
+        <div className="event--card card shadow">
+          <p className="badge bg-warning">Upcoming</p>
+          <main className="">
+            <h3>Serverless in Action</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Laborum deleniti illo culpa adipisci consequatur veniam quo iste!
+              Commodi, recusandae harum explicabo id repellat,
+              labore libero temporibus accusamus, excepturi beatae cupiditate!
+            </p>
+            <article>
+              <h6>Date: 26th Sep 2021</h6>
+            </article>
+            <button className="btn btn-primary">
+              Check Details
+            </button>
+          </main>
+          <main className="">
+            <img width="160" height="180"
+              src="https://p2zk82o7hr3yb6ge7gzxx4ki-wpengine.netdna-ssl.com/wp-content/uploads/2018/01/serverless_bze.png" alt="" />
+          </main>
+        </div>
       </section>
     </div>
   )
