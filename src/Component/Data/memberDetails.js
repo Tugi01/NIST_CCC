44ce128f02c195648da08d482bdaea06d2971227
Debarshi <debopiku1122@gmail.com>
2021-10-30 23:45:11 +0530

    update_member_details

--- src/Component/Data/memberDetails.js
+++ src/Component/Data/memberDetails.js
@@ -42,7 +42,7 @@ export const club_member_details = [
     firstname: 'Prasanth',
     fullname: 'Sriramadasu Prasanth Kumar',
     description: `Passionate towards data science, devops and cloud computing. Worked on many clouds like AWS, Azure, GCP, OpenStack and automation technologies like devOps.`,
-    desgination: 'Machine Learning, DevOps and Cloud Computing',
+    desgination: 'Machine Learning, DevOps & Cloud Computing',
     member_type: 'Core Member',
     profile_img: '',
     linkedln_link: `https://www.linkedin.com/in/sriramadasu-prasanth-kumar-917703184/`,
