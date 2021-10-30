474843d89dcf975627c0ec3a720c553167397b34
Debarshi <debopiku1122@gmail.com>
2021-10-30 23:48:59 +0530

    update_member_details

--- src/Pages/Events/EventPage/OneEvent.js
+++ src/Pages/Events/EventPage/OneEvent.js
@@ -1,6 +1,5 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom';
-import { useState } from 'react/cjs/react.development';
 import { eventDetails } from '../../../Component/Data/eventDetails';
 
 
