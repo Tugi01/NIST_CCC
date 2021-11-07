1d74cde745358376ae586b5bd353cc4d2a377c65
Sutari Sunil Reddy <66204882+Sunilreddy0@users.noreply.github.com>
2021-11-07 20:33:30 +0530

    Update eventDetails.js

--- src/Component/Data/eventDetails.js
+++ src/Component/Data/eventDetails.js
@@ -2,9 +2,7 @@ export const eventDetails = [
   {
     id: 1,
     name: 'Serverless in Action',
-    describe: `Lorem ipsum dolor sit amet consectetur adipisicing elit.
-    Laborum deleniti illo culpa adipisci consequatur veniam quo iste!
-    Commodi, recusandae harum explicabo id repellat.`,
+    describe: `Microservices architecture is a distributed design approach intended to overcome the limitations of traditional monolithic architectures.`,
     date: '26-10-2021',
     time: '',
     venue: 'Will Update',
