80b0072c20cadcb5393df77f73c4791daa286fca
Sutari Sunil Reddy <66204882+Sunilreddy0@users.noreply.github.com>
2021-11-07 20:27:50 +0530

    Update techDrive.js

--- src/Component/Data/techDrive.js
+++ src/Component/Data/techDrive.js
@@ -4,23 +4,25 @@ export const tect_data = [
     img: 'https://www.slappforge.com/wp-content/uploads/2020/02/serverless-debugging-with-sigma-01.png',
     headTxt_normal: 'Computing',
     headTxt_bold: 'Serverless',
-    content: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi, eveniet. Odio commodi quia optio.
-    Esse rem dolorem possimus iure quidem porro facilis!`
+    content: `The term ‘serverless’ is somewhat misleading, as there are still servers providing these backend services, 
+    but all of the server space and infrastructure concerns are handled by the vendor. Serverless means that the developers 
+    can do their work without having to worry about servers at all.`
   },
   {
     id: 1,
     img: 'https://apifortress.com/wp-content/uploads/2020/09/WebServicesproductizedAPI-s-Hero.png',
     headTxt_normal: 'with Cloud Services',
     headTxt_bold: 'Automation',
-    content: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi, eveniet. Odio commodi quia optio.
-    Esse rem dolorem possimus iure quidem porro facilis!`
+    content: `One of the biggest benefits of a cloud move is the opportunity to reduce toil and operating costs, and 
+    one of the most important elements is the automation. By automating the provisioning, and management of your cloud-based 
+    infrastructure, your organization can free up time and resources for mission-critical innovation instead of routine maintenance.`
   },
   {
     id: 2,
     img: 'https://psystems.co/wp-content/uploads/2020/04/02-4-1.png',
     headTxt_normal: 'Cloud Native',
     headTxt_bold: 'Development',
-    content: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi, eveniet. Odio commodi quia optio.
-    Esse rem dolorem possimus iure quidem porro facilis!`
+    content: `Cloud native applications deliver faster time to market, higher scalability, simpler management and reduce cost through 
+    containerization, microservices, automation and CI/CD.`
   },
 ]
